b99586e4ae62205286a4be4fd3ec204cd4fe891b
Ryan Batty <rgbatty@outlook.com>
2022-07-20 18:41:18 -0600

    feat(vscode): add extension placeholders and setting definitions

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -11,12 +11,61 @@ in {
       enable = true;
 
       extensions = with pkgs.vscode-extensions; [
+        # TODO: Evaluate these for nixpkg equivalents
+        # aaron-bond.better-comments
+        # BriteSnow.vscode-toggle-quotes
 
+        # TODO: these don't build... for some reason
+        # bbenoist.nix
+        # christian-kohler.path-intellisense
+        # dracula-theme.theme-dracula
+        # eamodio.gitlens
+        # gruntfuggly.todo-tree
+        # ms-azuretools.vscode-docker
+        # ms-vsliveshare.vsliveshare
+        # pkief.material-icon-theme
+        # skellock.just
+        # skyapps.fish-vscode
       ];
 
       userSettings = {
+        breadcrumbs.enabled = true;
+
+        editor.accessibilitySupport = "off";
+        editor.bracketPairColorization.enabled = true;
+        editor.cursorBlinking = "phase";
+        editor.cursorStyle = "line";
+        editor.dragAndDrop = false;
+        editor.fontFamily = "'FiraCode', Consolas, 'Courier New', monospace";
+        editor.fontLigatures = true;
+        editor.fontSize = 15;
+        # TODO: Re-evaluate formatOnSave (per-project?)
+        # editor.formatOnSave = false;
+        editor.glyphMargin = false;
+        editor.lineHeight = 22;
+        editor.linkedEditing = true;
+        editor.minimap.enabled = false;
+        editor.tabSize = 2;
+
+        explorer.autoReveal = true;
+        explorer.confirmDragAndDrop = false;
+        explorer.confirmDelete = false;
         extensions.autoUpdate = true;
+
+        files.autoSave = "afterDelay";
+        files.autoSaveDelay = 100;
+        files.insertFinalNewline = true;
+        files.trimTrailingWhitespace = true;
+
         git.autofetch = false;
+        git.inputValidationSubjectLength = 72;
+
+        html.format.enable = true;
+        html.format.endWithNewline = false;
+
+        markdown.preview.scrollEditorWithPreview = true;
+        markdown.preview.scrollPreviewWithEditor = true;
+
         sync.askGistName = false;
         sync.autoDownload = false;
         sync.autoUpload = false;
@@ -26,13 +75,30 @@ in {
         sync.pathPrefix = "";
         sync.quietSync = false;
         sync.removeExtensions = true;
+
         telemetry.enableCrashReporter = false;
         telemetry.enableTelemetry = false;
+
+        # TODO: evaluate terminal settings for Darwin
+        # terminal.explorerKind = "external";
+        # terminal.external.osxExec = "iTerm.app";
+        # terminal.integrated.fontFamily = "Inconsolata for Powerline"; // Install Powerline fonts for this to work
+        # terminal.integrated.fontSize = 12;
+        # terminal.integrated.shell.osx = "fish";
+
+        window.title = "${activeEditorMedium}${separator}${rootName}";
         window.zoomLevel = 1;
-        # workbench.colorTheme = "";
-        # workbench.enableExperiments = false;
-        # workbench.settings.enableNaturalLanguageSearch = false;
-        # workbench.statusBar.feedback.visible = false;
+        workbench.colorTheme = "Dracula";
+
+        workbench.editor.enablePreview = false;
+        workbench.editor.highlightModifiedTabs = true;
+        workbench.editor.tabCloseButton = "right";
+        workbench.editor.tabSizing = "shrink";
+        workbench.enableExperiments = false;
+        workbench.panel.defaultLocation = "right";
+        workbench.settings.enableNaturalLanguageSearch = false;
+        workbench.statusBar.feedback.visible = false;
+        workbench.sideBar.location = "right";
       };
     };
   };
