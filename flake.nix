c4b37f346fd58551b8978c35f93eee4745bed77b
Ryan Batty <rgbatty@outlook.com>
2022-07-24 21:44:56 -0600

    feat(channels): use unstable builds for nix-darwin

--- flake.nix
+++ flake.nix
@@ -6,14 +6,12 @@
     agenix.inputs.nixpkgs.follows = "nixpkgs";
 
     darwin.url = "github:LnL7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    # nixpkgs-arm.url = "nixpkgs/nixos-22.05-aarch64";
-    nixpkgs-darwin.url = "nixpkgs/nixpkgs-22.05-darwin";
     nixpkgs-master.url = "nixpkgs/master";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
 
