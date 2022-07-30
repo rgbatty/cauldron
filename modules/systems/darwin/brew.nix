9b1417767241dcbc9c2d0a6c057963795523c1e5
Ryan Batty <rgbatty@outlook.com>
2022-07-30 16:15:32 -0600

    fix(brew): remove escape character

--- modules/systems/darwin/brew.nix
+++ modules/systems/darwin/brew.nix
@@ -1,6 +1,6 @@
 { inputs, config, lib, pkgs, ... }: {
   environment.interactiveShellInit = ''
-    eval "$\(/opt/homebrew/bin/brew shellenv)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   '';
 
   homebrew = {
