4e04231352a3700c723feba66ea9a691a393abec
Ryan Batty <rbatty@koumori.dev>
2023-04-05 14:20:11 -0600

    fix: temp disable home manager application workaround (may be fixed already)

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -18,26 +18,26 @@ with lib;
       packages = lib.optionals (pkgs.stdenv.isLinux) [ pkgs.xdg_utils ];
       stateVersion = "22.11";
 
-      activation = mkIf pkgs.stdenv.isDarwin {
-        copyApplications = let
-          apps = pkgs.buildEnv {
-            name = "home-manager-applications";
-            paths = config.home.packages;
-            pathsToLink = "/Applications";
-          };
-        in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          baseDir="$HOME/Applications/Home Manager Apps"
-          if [ -d "$baseDir" ]; then
-            rm -rf "$baseDir"
-          fi
-          mkdir -p "$baseDir"
-          for appFile in ${apps}/Applications/*; do
-            target="$baseDir/$(basename "$appFile")"
-            $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
-            $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
-          done
-        '';
-        };
+      # activation = mkIf pkgs.stdenv.isDarwin {
+      #   copyApplications = let
+      #     apps = pkgs.buildEnv {
+      #       name = "home-manager-applications";
+      #       paths = config.home.packages;
+      #       pathsToLink = "/Applications";
+      #     };
+      #   in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      #     baseDir="$HOME/Applications/Home Manager Apps"
+      #     if [ -d "$baseDir" ]; then
+      #       rm -rf "$baseDir"
+      #     fi
+      #     mkdir -p "$baseDir"
+      #     for appFile in ${apps}/Applications/*; do
+      #       target="$baseDir/$(basename "$appFile")"
+      #       $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+      #       $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+      #     done
+      #   '';
+      #   };
     };
 
     manual.manpages.enable = true;
